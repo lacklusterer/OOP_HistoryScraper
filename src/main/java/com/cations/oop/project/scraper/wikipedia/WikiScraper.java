4ef78f3bbae5fa0c8a1c31d2df61cb94f87e683f
lacklusterer <minh.nn210582@sis.hust.edu.vn>
2023-07-10 02:18:43 +0700

    WikipediaKing: Fixed json output

--- src/main/java/com/cations/oop/project/scraper/wikipedia/WikiScraper.java
+++ src/main/java/com/cations/oop/project/scraper/wikipedia/WikiScraper.java
@@ -1,9 +1,7 @@
 package com.cations.oop.project.scraper.wikipedia;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -12,7 +10,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Objects;
 
-public class WikiScraper extends BaseScrapper{
+public class WikiScraper extends BaseScrapper {
 
     @Override
     protected void processDoc(Document document) {
@@ -22,38 +20,32 @@ public class WikiScraper extends BaseScrapper{
         // Get data from info box
         Element infoBox = document.selectFirst(".infobox");
 
-        // Output found data - currently into console
+        // Output found data into json
         if (infoBox != null) {
             Elements rows = infoBox.select("tr");
 
             // Create a Gson instance
             Gson gson = new Gson();
 
-            // Create a JsonArray to hold the rows
-            JsonArray jsonArray = new JsonArray();
+            // Create a JsonObject to hold the data
+            JsonObject dataObject = new JsonObject();
 
-            // Iterate over the rows
+            // Add name to the data JsonObject
+            dataObject.addProperty("Tên ", name);
+
+            // Iterate over the infobox's rows
             for (Element row : rows) {
                 Element label = row.selectFirst("th");
                 Element value = row.selectFirst("td");
 
                 if (label != null && value != null) {
-                    String labelText = label.text();
-                    String valueText = value.text();
-
-                    // Create a JsonObject for each row
-                    JsonObject rowObject = new JsonObject();
-                    rowObject.addProperty("label", labelText);
-                    rowObject.addProperty("value", valueText);
-
-                    // Add the row JsonObject to the JsonArray
-                    jsonArray.add(rowObject);
+                    dataObject.addProperty(label.text(), value.text());
                 }
             }
 
-            // Write the JSON data to a file
+            // Write the JSON data to file
             try (FileWriter writer = new FileWriter("out/wiki/wikiScrappedKing/output.json")) {
-                gson.toJson(jsonArray, writer);
+                gson.toJson(dataObject, writer);
             } catch (IOException e) {
                 e.printStackTrace();
             }
