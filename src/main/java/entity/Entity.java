5eb0ca07b6bc69860dc0f0e820ab729f4590ec72
Martin Cation <martincation@protonmail.com>
2023-07-17 14:51:02 +0700

    entity: base class

---
+++ src/main/java/entity/Entity.java
@@ -0,0 +1,25 @@
+package entity;
+
+/**
+ * Base entity to be parsed into by scrappers.
+ * Contains a name, and equals() will be check through this field.
+ */
+public abstract class Entity {
+	private String name;
+	private String source;
+
+	protected Entity(String name, String source) {
+		this.name = name;
+		this.source = source;
+	}
+
+	public String getName() { return name; }
+	public String getSource() { return source; }
+
+	@Override
+	public boolean equals(Object other) {
+		var otherEntity = (Entity)other;
+		return name.equals(otherEntity.name) &&
+			source.equals(otherEntity.source);
+	}
+}
