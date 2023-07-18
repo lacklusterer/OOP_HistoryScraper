b809eb22b4b497d594ee154a6984c784e0c87177
Martin Cation <martincation@protonmail.com>
2023-07-18 13:47:12 +0700

    scraper/wikipedia: set base class visibility to package-only

--- src/main/java/scraper/wikipedia/Base.java
+++ src/main/java/scraper/wikipedia/Base.java
@@ -2,7 +2,10 @@ package scraper.wikipedia;
 
 import scraper.Page;
 
-public abstract class Base extends Page {
+/**
+ * Abstract package-visible base to provide a base url.
+ */
+abstract class Base extends Page {
 	protected final static String baseUrl = "https://vi.wikipedia.org";
 
 	protected Base(String url) { super(url); }
